fca6f5f756cb8c91d7ab18fd75140e5c830432f1
AksCode333 <47060340+AksCode333@users.noreply.github.com>
2024-11-09 16:03:07 -0600

    Update MathFunction.py
    Simple equation

--- MathFunction.py
+++ MathFunction.py
@@ -1,1 +1,4 @@
+#Equation (h = g × t2/2)
+print((i/1000), "seconds    ", (9.81 * (i/1000) * (i/1000) / 2) * 12, "Inches")
 
+#this is the whole equation to take the raw value of hang time and convert it into an inches measurement
